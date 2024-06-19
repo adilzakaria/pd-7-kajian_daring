64aff2b1df456315c9d545decf88fa560a3d7eba
Adil Zakaria <adilzakaria18@gmail.com>
2024-06-19 12:00:47 +0700

    import template dan install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,47 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/home', function () {
+    return view('frontend.home');
+});
+Route::get('/about', function () {
+    return view('frontend.about');
+});
+Route::get('/class', function () {
+    return view('frontend.class');
+});
+Route::get('/teachers', function () {
+    return view('frontend.teachers');
+});
+Route::get('/gallery', function () {
+    return view('frontend.gallery');
+});
+Route::get('/pages', function () {
+    return view('frontend.pages');
+});
+Route::get('/blog', function () {
+    return view('frontend.blog');
+});
+Route::get('/single', function () {
+    return view('frontend.single');
+});
+Route::get('/contact', function () {
+    return view('frontend.contact');
 });
